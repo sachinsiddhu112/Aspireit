ba57ac0d791bb62e126b40e105aeacf71e4a6433
Sachin <sachinsiddhu112@gmail.com>
2024-06-11 16:18:43 +0530

    solve one error

--- app/backend/routes/auth.js
+++ app/backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user");
+const User = require("../models/User.js");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
